ae5b85f2bc258128a3ee59f28d20873dfe20fd9c
Shuning Lin <shuningl@andrew.cmu.edu>
2023-11-13 01:50:57 -0500

    draft

--- examples/round_trip/android-client/app/src/main/java/edu/cmu/cs/roundtrip/GabrielActivity.java
+++ examples/round_trip/android-client/app/src/main/java/edu/cmu/cs/roundtrip/GabrielActivity.java
@@ -1,8 +1,23 @@
 package edu.cmu.cs.roundtrip;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.hardware.Camera;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.media.AudioRecord;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.MediaController;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +27,7 @@ import androidx.camera.view.PreviewView;
 
 import com.google.protobuf.ByteString;
 
+import java.io.ByteArrayOutputStream;
 import java.util.function.Consumer;
 
 import edu.cmu.cs.gabriel.camera.CameraCapture;
@@ -34,25 +50,54 @@ public class GabrielActivity extends AppCompatActivity {
     private YuvToJPEGConverter yuvToJPEGConverter;
     private CameraCapture cameraCapture;
 
+    private PreviewView previewView = null;
+    private TextView textView = null;
+
+    private EditText editText = null;
+
+    private Button button = null;
+
+    private String typed_string = "";
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_gabriel);
 
-        PreviewView viewFinder = findViewById(R.id.viewFinder);
-        ImageView imageView = findViewById(R.id.imageView);
-        ImageViewUpdater imageViewUpdater = new ImageViewUpdater(imageView);
+        previewView = findViewById(R.id.preview);
+        textView = findViewById(R.id.textAnnotation);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.startAnnotationButton);
+
+
+        // Set a click listener on the button
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                typed_string = editText.getText().toString();
+                editText.getText().clear();
+                serverComm.sendSupplier(() -> {
+                    Bitmap bitmap = previewView.getBitmap();
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG,80,stream);
+                    return InputFrame.newBuilder().setPayloadType(PayloadType.IMAGE)
+                            .addPayloads(ByteString.copyFrom(stream.toByteArray()))
+                            .build();
+                }, SOURCE, false);
+            }
+        });
+
 
         Consumer<ResultWrapper> consumer = resultWrapper -> {
             if (resultWrapper.getResultsCount() == 0) {
                 return;
             }
-
             ResultWrapper.Result result = resultWrapper.getResults(0);
-            ByteString jpegByteString = result.getPayload();
-
-            imageViewUpdater.accept(jpegByteString);
+            ByteString byteString = result.getPayload();
+            textView.setText(byteString.toStringUtf8());
         };
 
         Consumer<ErrorType> onDisconnect = errorType -> {
@@ -64,7 +109,7 @@ public class GabrielActivity extends AppCompatActivity {
                 consumer, BuildConfig.GABRIEL_HOST, PORT, getApplication(), onDisconnect);
 
         yuvToJPEGConverter = new YuvToJPEGConverter(this);
-        cameraCapture = new CameraCapture(this, analyzer, WIDTH, HEIGHT, viewFinder);
+        cameraCapture = new CameraCapture(this, analyzer, WIDTH, HEIGHT, previewView);
     }
 
     final private ImageAnalysis.Analyzer analyzer = new ImageAnalysis.Analyzer() {
